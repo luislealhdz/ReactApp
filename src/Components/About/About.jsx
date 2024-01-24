5cdf969478163ac8a7d90fda4c9ac491da796f9a
0HungryDeveloper <hungrystudentdev@gmail.com>
2024-01-24 13:59:43 -0600

    About component structure

--- src/Components/About/About.jsx
+++ src/Components/About/About.jsx
@@ -1,5 +1,41 @@
 const About = () => {
-    return <div>About</div>;
+    return (
+        <section>
+            <article>
+                <div>
+                    <img
+                        src="https://avatars.githubusercontent.com/u/95981661?v=4"
+                        alt="Foto de perfil"
+                    />
+                </div>
+            </article>
+            <aside>
+                <h1>Luis Leal</h1>
+                <p>
+                    <b>A passionate frontend developer from México</b>
+                </p>
+                <ul>
+                    <li>Lorem ipsum dolor sit.</li>
+                    <li>Lorem ipsum dolor sit.</li>
+                    <li>Lorem ipsum dolor sit.</li>
+                </ul>
+                <p>
+                    <b>Connect with me</b>
+                </p>
+                <p>Linkedin</p>
+                <p>Github</p>
+                <p>Gmail</p>
+
+                <section>
+                    <p>Languages and Tools</p>
+                    <ul>
+                        <li>HTML</li>
+                        <li>CSS</li>
+                    </ul>
+                </section>
+            </aside>
+        </section>
+    );
 };
 
 export default About;
